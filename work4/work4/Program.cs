f11b2cd1ba2340f1d549c159fe169e42df91ec9b
ljcllll <1097506252@qq.com>
2020-03-16 21:42:37 +0800

    not done

---
+++ work4/work4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { set; get; }
+        public T Data { set; get; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> x = head; x != null; x = x.Next)
+            {
+                action(x.Data);
+            }
+        }
+    }
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for(int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            for(Node<int> node = intlist.Head; node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+            intlist.ForEach(x => Console.WriteLine(x));
+            int sum = 0, max = 0, min = 0;
+            intlist.ForEach(x => sum += x);
+            intlist.ForEach(x => { if (max < x) max = x; });
+            intlist.ForEach(x => { if (min > x) min = x; });
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ work4/work4_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace work4_2
+{
+    public delegate void AlarmHandler(int timeTick);
+    public delegate void TickHandler(int timeTick);
+
+    class ClockEvent
+    {
+        public event AlarmHandler onAlarm;
+        public event TickHandler onTick;
+        public void Tick(int timeTick) { onTick(timeTick); }
+        public void Alarm(int timeTick) { onAlarm(timeTick); }
+    }
+
+    class AlarmClock
+    {
+        public ClockEvent clock = new ClockEvent();
+        //private int timeTick;
+        public int timeTick { set; get; }
+        public AlarmClock()
+        {
+            timeTick = 0;
+            clock.onTick += new TickHandler(TickEvent);
+            clock.onAlarm += new AlarmHandler(AlarmEvent);
+        }
+
+        public void TickEvent(int timeTick)
+        {
+            Console.WriteLine("Tick at time" + timeTick);
+        }
+        public void AlarmEvent(int timeTick)
+        {
+            Console.WriteLine("Alarm at time" + timeTick);
+        }
+        public void Tick()
+        {
+            clock.Tick(timeTick);
+        }
+        public void Alarm(){
+            clock.Alarm(timeTick);
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                AlarmClock alarmClock = new AlarmClock();
+                alarmClock.timeTick
+        }
+    }
+}
